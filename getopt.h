887c5e1ecec6f1fd45ae1f0ded9ba38636574c3b
Dmitry Lyzin <dmitry_lyzin@mail.ru>
2021-10-07 20:30:57 +0300

    Чертов C++

--- getopt.h
+++ getopt.h
@@ -1,12 +1,20 @@
 #ifndef GETOPT_H
 #define GETOPT_H
 
-extern int opterr;	/* if error message should be printed */
-extern int optind;	/* index into parent argv vector */
-extern int optopt;	/* character checked for validity */
-extern int optreset;	/* reset getopt  */
-extern char *optarg;	/* argument associated with option */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-int getopt(int nargc, const char * const nargv[], const char *ostr);
+	extern int opterr;	/* if error message should be printed */
+	extern int optind;	/* index into parent argv vector */
+	extern int optopt;	/* character checked for validity */
+	extern int optreset;	/* reset getopt  */
+	extern char* optarg;	/* argument associated with option */
+
+	int getopt( int nargc, const char* const nargv[], const char* ostr );
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
