b5136eb1aa86015b30b9e9efbe06b5d90c69cbd3
Dmitry Lyzin <dmitry_lyzin@mail.ru>
2021-10-07 19:13:28 +0300

    Initial commit

---
+++ testopt.c
@@ -0,0 +1,57 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __unix__
+#	include <unistd.h>
+#else
+#	include "getopt.h"
+#endif
+
+int
+main(int argc, const char *argv[])
+{
+	int aflag = 0;
+	int bflag = 0;
+	char *cvalue = NULL;
+	int index;
+	int c;
+
+	opterr = 0;
+
+	while( (c = getopt( argc, argv, "abc:" )) != -1 )
+	{
+		switch( c )
+		{
+		case 'a':
+			aflag = 1;
+			break;
+		case 'b':
+			bflag = 1;
+			break;
+		case 'c':
+			cvalue = optarg;
+			break;
+		case '?':
+			if( optopt == 'c' )
+				fprintf( stderr, "Option -%c requires an argument.\n", optopt );
+			else if( isprint( optopt ) )
+				fprintf( stderr, "Unknown option `-%c'.\n", optopt );
+			else
+				fprintf( stderr,
+					"Unknown option character `\\x%x'.\n",
+					optopt );
+			return 1;
+		default:
+			abort();
+		}
+	}
+
+	printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+		aflag, bflag, cvalue);
+
+	for (index = optind; index < argc; index++)
+		printf ("Non-option argument %s\n", argv[index]);
+
+	return 0;
+}
